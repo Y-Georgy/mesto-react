cea8e0ebaa15b717efb6baab90452a52e0184144
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-08-20 21:20:24 +0300

    function popups open

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,25 @@
 export const Main = () => {
+  function handleEditAvatarClick () {
+    const popupEditAvatar = document.querySelector('.popup_type_avatar');
+    popupEditAvatar.classList.add('popup_opened');
+  };
+
+  function handleEditProfileClick () {
+    const popupEditProfile = document.querySelector('.popup_type_edit');
+    popupEditProfile.classList.add('popup_opened');
+  };
+
+  function handleAddPlaceClick () {
+    const popupAddPlace = document.querySelector('.popup_type_add');
+    popupAddPlace.classList.add('popup_opened');
+  };
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__author">
           <img className="profile__avatar" src="#" alt="Аватар автора" />
-          <div className="profile__overlay-avatar"></div>
+          <div className="profile__overlay-avatar" onClick={handleEditAvatarClick}></div>
           <div className="profile__text">
             <div className="profile__title-overlay">
               <h1 className="profile__title"></h1>
@@ -12,6 +27,7 @@ export const Main = () => {
                 type="button"
                 className="profile__edit-button"
                 aria-label="Редактировать"
+                onClick={handleEditProfileClick}
               ></button>
             </div>
             <p className="profile__subtitle"></p>
@@ -21,6 +37,7 @@ export const Main = () => {
           type="button"
           className="profile__add-button"
           aria-label="Добавить"
+          onClick={handleAddPlaceClick}
         ></button>
       </section>
       <section className="elements">
