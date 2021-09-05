3880594e4b0f39fa263f072d1c58ddab183bbc38
Georgy Yadrihinskiy <y-georgy@yandex.ru>
2021-09-05 10:11:18 +0300

    fix: loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,14 @@
 function Loading() {
   return (
     <div id="loading">
-      <div id="loading_1" class="loading"></div>
-      <div id="loading_2" class="loading"></div>
-      <div id="loading_3" class="loading"></div>
-      <div id="loading_4" class="loading"></div>
-      <div id="loading_5" class="loading"></div>
-      <div id="loading_6" class="loading"></div>
-      <div id="loading_7" class="loading"></div>
-      <div id="loading_8" class="loading"></div>
+      <div id="loading_1" className="loading"></div>
+      <div id="loading_2" className="loading"></div>
+      <div id="loading_3" className="loading"></div>
+      <div id="loading_4" className="loading"></div>
+      <div id="loading_5" className="loading"></div>
+      <div id="loading_6" className="loading"></div>
+      <div id="loading_7" className="loading"></div>
+      <div id="loading_8" className="loading"></div>
     </div>
   )
 }
